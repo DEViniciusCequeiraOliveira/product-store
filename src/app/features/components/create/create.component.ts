d6a55da89b8c3503fe81c69cd3995c49b2dd1791
Vinicius Cerqueira Oliveira <dev.vinicius.c.oliveira@gmail.com>
2024-05-26 16:37:32 -0300

    criando o formulario

--- src/app/features/components/create/create.component.ts
+++ src/app/features/components/create/create.component.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import {  MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-create',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatButtonModule],
   templateUrl: './create.component.html',
   styleUrl: './create.component.scss'
 })
-export class CreateComponent {
 
+export class CreateComponent {
+  form = new FormGroup({
+    title: new FormControl<String>('', {
+      nonNullable: true,
+      validators: Validators.required,
+    }),
+  });
+
+  onSubmit() {
+    this.form.controls.title;
+  }
 }
