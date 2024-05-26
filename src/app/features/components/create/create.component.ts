645f76df2632f03529a79de447c3b3f2c4f37951
Vinicius Cerqueira Oliveira <dev.vinicius.c.oliveira@gmail.com>
2024-05-26 17:23:07 -0300

    criando produtos

--- src/app/features/components/create/create.component.ts
+++ src/app/features/components/create/create.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import {  MatButtonModule } from '@angular/material/button';
+import { MatButtonModule } from '@angular/material/button';
+import { ProductsService } from '../../../shared/services/products.service';
+import { ProductPayload } from '../../../shared/interfaces/product-payload.interface';
 
 @Component({
   selector: 'app-create',
@@ -17,14 +19,36 @@ import {  MatButtonModule } from '@angular/material/button';
 })
 
 export class CreateComponent {
+
+  productService = inject(ProductsService);
+
   form = new FormGroup({
     title: new FormControl<String>('', {
+      nonNullable: true,
+      validators: Validators.required,
+    }),
+    price: new FormControl<Number>(0, {
+      nonNullable: true,
+      validators: Validators.required,
+    }),
+    description: new FormControl<String>('', {
+      nonNullable: true,
+      validators: Validators.required,
+    }),
+    image: new FormControl<String>('', {
       nonNullable: true,
       validators: Validators.required,
     }),
   });
 
   onSubmit() {
-    this.form.controls.title;
+    this.productService.post({
+      title: this.form.controls.title.value,
+      price: 13.5,
+      description: 'descrição do produto',
+      image: 'https://i.ibb.co/cvpntL1/hats.png'
+    }).subscribe((e) => {
+      alert('Cadastrado com sucesso!');
+    });
   }
 }
